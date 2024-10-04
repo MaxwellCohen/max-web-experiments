78ccbc7f3a8b848c005ecee3f608837a47a43512
Max Cohen <to.email.max@gmail.com>
2024-10-04 18:54:53 -0500

    ipx updates

--- src/app/api/ipx/[...slug]/route.ts
+++ src/app/api/ipx/[...slug]/route.ts
@@ -1,19 +1,20 @@
-import { withLeadingSlash, hasProtocol, joinURL, decode } from 'ufo';
-import { write } from 'node:fs'
+// port of the unjs/IPX server for next js source https://github.com/unjs/ipx/blob/main/src/server.ts#L3 
+
+import {  decode } from 'ufo';
 import { negotiate } from "@fastify/accept-negotiator";
 import {
   createIPX,
   ipxFSStorage,
   ipxHttpStorage,
 } from "ipx";
-
+import getEtag from "etag";
 
 const ipx = createIPX({
   storage: ipxFSStorage({
     dir: "./picture"
   }),
   httpStorage: ipxHttpStorage({
-    domains: ["utfs.io"]
+    domains: []
   }),
 });
 
@@ -22,15 +23,6 @@ function safeString(input: unknown) {
   return JSON.stringify(input).replace(/^"|"$/g, "").replace(/\\+/g, "\\").replace(/\\"/g, '"');
 }
 
-class MyError extends Error {
-  constructor(
-    public statusCode: number,
-    public statusText: string,
-    public message: string,
-  ) {
-    super(message)
-  }
-}
 const MODIFIER_SEP = /[&,]/g;
 const MODIFIER_VAL_SEP = /[:=_]/;
 
@@ -51,8 +43,7 @@ function autoDetectFormat(acceptHeader: string, animated: boolean) {
   return acceptMime?.split("/")[1] ?? "jpeg";
 }
 
-
-function getModifiersFromString (modifiersString: string) {
+function getModifiersFromString(modifiersString: string, acceptHeader: string, headers: Record<string, string>) {
   // Contruct modifiers
   const modifiers: Record<string, string> = Object.create(null) as Record<string, string>;
 
@@ -65,34 +56,81 @@ function getModifiersFromString (modifiersString: string) {
         .join("_");
     }
   }
-  return modifiers;
+  // Auto format
+  const mFormat = modifiers.f ?? modifiers.format;
+  if (mFormat === "auto") {
 
+    const autoFormat = autoDetectFormat(
+      acceptHeader,
+      !!(modifiers.a ?? modifiers.animated),
+    );
+    delete modifiers.f;
+    delete modifiers.format;
+    if (autoFormat) {
+      modifiers.format = autoFormat;
+      headers.vary = "Accept";
+    }
+  }
+  return modifiers;
 }
-type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
-type ipxOptions = Prettify<Parameters<typeof ipx>[1]>
 
 
 export async function GET(req: NextRequest) {
-  const settings: ResponseInit = { headers: {}}
+  const settings = { headers: {} as Record<string, string> }
   const [, path = ''] = req.url.split('api/ipx/')
   const [modifiersString = "", ...idSegments] = path.split("/");
   const id = safeString(decode(idSegments.join("/")));
-  const modifiers = getModifiersFromString(modifiersString);
-    // Create request
-    console.log(id)
-    console.log(modifiers)
-    const img = ipx(id, modifiers);
 
-    // Get image meta from source
-    const sourceMeta = await img.getSourceMeta();
-    
-    // Process image
-    const { data, format } = await img.process();
-    const response = new NextResponse(data);
-    response.headers.set('vary', "Accept") 
-    response.headers.set( "content-type", `image/${format}`) 
+  // Validate
+  if (!modifiersString) {
+    return new NextResponse(`Modifiers are missing: ${id}`, { status: 400, statusText: `IPX_MISSING_MODIFIERS` });
+  }
+  if (!id || id === "/") {
+    return new NextResponse(`Resource id is missing: ${path}`, { status: 400, statusText: `IPX_MISSING_ID` });
+  }
+  const modifiers = getModifiersFromString(modifiersString, req.headers.get('accept') ?? '', settings.headers)
+
+  // Create request
+  const img = ipx(id, modifiers);
+
+  // Get image meta from source
+  const sourceMeta = await img.getSourceMeta();
+  // Handle modified time if available
+  if (sourceMeta.mtime) {
+    // Send Last-Modified header
+    settings.headers["last-modified"] = req.headers.get("last-modified") ?? sourceMeta.mtime.toUTCString();
+
+    // Check for last-modified request header
+    const _ifModifiedSince = req.headers.get("if-modified-since");
+    if (_ifModifiedSince && new Date(_ifModifiedSince) >= sourceMeta.mtime) {
+      return new NextResponse(null, { status: 304 })
+    }
+  }
+
+  // Process image
+  const { data, format } = await img.process();
+  const response = new NextResponse(data, settings);
+
+  // Send Cache-Control header
+  if (typeof sourceMeta.maxAge === "number") {
+    response.headers.set("cache-control", req.headers.get("cache-control") ?? `max-age=${+sourceMeta.maxAge}, public, s-maxage=${+sourceMeta.maxAge}`);
+  }
+
+  // Generate and send ETag header
+  const etag = getEtag(data);
+  response.headers.set("etag", req.headers.get("etag") ?? etag);
+
+  // Check for if-none-match request header
+  if (etag && req.headers.get("if-none-match") === etag) {
+    return new NextResponse(null, { status: 304 })
+  }
+
+
+  if (format) {
+    response.headers.set("content-type", `image/${format}`);
+  }
+  response.headers.set("content-security-policy", req.headers.get("content-security-policy") ?? "default-src 'none'");
+
   return response;
 }
 
