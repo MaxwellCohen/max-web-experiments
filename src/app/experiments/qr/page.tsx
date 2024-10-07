4bdac716e9bb6d4f2f390d87e65f83a904f92568
Max Cohen <to.email.max@gmail.com>
2024-10-07 07:22:53 -0500

    add suspense bouldery

--- src/app/experiments/qr/page.tsx
+++ src/app/experiments/qr/page.tsx
@@ -1,22 +1,23 @@
 import { type Metadata } from "next";
+import { Suspense } from "react";
 import { QRForm } from "@/components/qrForm";
-
-
 
 export const metadata: Metadata = {
   title: "Base 64 Encoder / Decoder",
   description: "fit images into a div regardless of the size of the image",
 };
 
-
 export default function Base64Page() {
-
   return (
     <>
       <h1 className="text-2xl font-bold">QR generator</h1>
-      <p>Qr Generator powered by UnJS&apos;s <a href="https://github.com/unjs/uqr">uqr</a></p>
-      <QRForm />
+      <p>
+        Qr Generator powered by UnJS&apos;s{" "}
+        <a href="https://github.com/unjs/uqr">uqr</a>
+      </p>
+      <Suspense fallback={null}>
+        <QRForm />
+      </Suspense>
     </>
   );
 }
-
